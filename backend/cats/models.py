7860725de86b72da89a31cd5c834cf54bede183d
vinni-pushinka <valerygoran@gmail.com>
2023-07-29 17:44:47 +0000

    Minor improvements

--- backend/cats/models.py
+++ backend/cats/models.py
@@ -6,25 +6,29 @@ User = get_user_model()
 
 
 class Achievement(models.Model):
-    name = models.CharField(max_length=64)
+    '''Класс для представления достижения кота.'''
+    name = models.CharField(
+        max_length=64, verbose_name='Достижение')
 
     def __str__(self):
         return self.name
 
 
 class Cat(models.Model):
-    name = models.CharField(max_length=16)
-    color = models.CharField(max_length=16)
-    birth_year = models.IntegerField()
+    '''Класс для представления кота.'''
+    name = models.CharField(max_length=16, verbose_name='Имя')
+    color = models.CharField(max_length=16, verbose_name='Окрас')
+    birth_year = models.IntegerField(verbose_name='Год рождения')
     owner = models.ForeignKey(
-        User, related_name='cats', 
+        User, related_name='cats',
         on_delete=models.CASCADE
         )
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+    achievements = models.ManyToManyField(
+        Achievement, through='AchievementCat')
     image = models.ImageField(
-        upload_to='cats/images/', 
-        null=True,  
-        default=None
+        upload_to='cats/images/',
+        null=True,
+        default=None,
         )
 
     def __str__(self):
@@ -32,6 +36,7 @@ class Cat(models.Model):
 
 
 class AchievementCat(models.Model):
+    '''Класс для связи между котом и достижением.'''
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -10,8 +10,8 @@ from cats.views import AchievementViewSet, CatViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'cats', CatViewSet)
-router.register(r'achievements', AchievementViewSet)
+router.register('cats', CatViewSet)
+router.register('achievements', AchievementViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
